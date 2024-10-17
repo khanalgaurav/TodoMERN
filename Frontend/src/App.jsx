8853cbc3c145696781ab261b2d212730143044ff
khanalgaurav <khanalgaurav96@gmail.com>
2024-10-17 19:17:10 +0545

    Frontend Statement

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="username">Username</label>
+        <input type="name" name="username" />
+        <label htmlFor="email">Email</label>
+        <input type="name" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="name" name="password" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
